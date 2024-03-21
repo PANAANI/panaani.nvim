eb65f39a7ec8f35aacec9599671ce51029c22604
PANAANI <joonaoskari@hotmail.com>
2024-03-21 08:59:44 +0200

    Initial commit to setup the repo
    This repo will contain my entire nvim configuration so that I can easily install it wherever I want to

---
+++ lua/jn/options.lua
@@ -0,0 +1,10 @@
+vim.opt.shiftwidth = 4				-- changes indentation length
+vim.opt.tabstop = 4					-- changes the length of tab
+vim.opt.fileencoding = "utf-8"		-- makes the file encoding utf-8 by default
+vim.opt.relativenumber = true		-- a setting for relative line numbers
+vim.opt.number = true				-- line numbers
+vim.opt.splitbelow = true			-- place any horizontal splits on the bottom of the screen
+vim.opt.splitright = true			-- same as above but for vertical splits
+vim.opt.wrap = false				-- always show lines as one long pötkylä
+vim.opt.undofile = true				-- allows nvim to create an 'undofile' which stores undo history so that it can persist through closing and reopening a file
+
